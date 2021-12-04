0a7102acd53dd6cb8cfaa529953ef43a6b049c46
David Nash <hisownspace@gmail.com>
2021-12-03 19:38:57 -0500

    user auth middleware testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+const { restoreUser } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 module.exports = router;
