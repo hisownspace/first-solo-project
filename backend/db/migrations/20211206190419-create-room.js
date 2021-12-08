37b2eb63ddb4a78f3ff85180c42de6782c20f609
David Nash <hisownspace@gmail.com>
2021-12-07 19:08:57 -0500

    altered db

--- backend/db/migrations/20211206190419-create-room.js
+++ backend/db/migrations/20211206190419-create-room.js
@@ -47,11 +47,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       }
     });
   },
